4d0c72dfd74010a81c5cc83607a238e8270a1e19
Kateryna Zhuk <105097557+Katia1324@users.noreply.github.com>
2022-07-20 20:29:29 +0200

    .;lkjhgfghjkl;lkjhgfghjk

--- lesson13/task5/index.test.js
+++ lesson13/task5/index.test.js
@@ -5,15 +5,34 @@ it ('should reverse massiv of numbers', () => {
     expect(result).toEqusal([5, 4, 3, 2, 1])
 });
 
+it('should reverse massiv of numbers', () => {
+  const result = reverseArray('1, 3, 5');
+  expect(result).toEqusal(null);
+});
+
+it('should reverse massiv of numbers', () => {
+  const result = reverseArray([1, 2, 3, 4, 5, 6, 8]);
+  expect(result).toEqusal([8, 6, 5, 4, 3, 2, 1]);
+});
+
+
+
 it('should withdraw funds from the account', () => {
   const result = withdraw(['Ann', 'John', 'User'], [1400, 87, -6], 'John', 50);
   expect(result).toEqusal(37);
 });
 
-it('should looking for people who are over 18 years old', () => {
+it('should withdraw funds from the account', () => {
+  const result = withdraw(['Ann', 'John', 'User'], [1400, 77, -6], 'John', 50);
+  expect(result).toEqusal(27);
+});
+
+it('should withdraw funds from the account', () => {
   const result = withdraw(['Ann', 'John', 'User'], [1400, 87, -6], 'User', 10);
   expect(result).toEqusal(-1);
 });
+
+
 
 it('should looking for people who are over 18 years old', () => {
   const result = getAdults({ Ann: 56, Andrey: 7 });
@@ -23,4 +42,9 @@ it('should looking for people who are over 18 years old', () => {
 it('should looking for people who are over 18 years old', () => {
   const result = getAdults({ 'John Doe': 19, Tom: 17, Bob: 18 });
   expect(result).toEqusal({ 'John Doe': 19, Bob: 18 });
+});
+
+it('should looking for people who are over 18 years old', () => {
+  const result = getAdults({ 'John Doe': 19, Tom: 28, Bob: 18 });
+  expect(result).toEqusal({ 'John Doe': 19, Tom: 28, Bob: 18 });
 });
