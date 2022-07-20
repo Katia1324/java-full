7a0a97e48482f3a19d848221bd403e8549555d29
Kateryna Zhuk <105097557+Katia1324@users.noreply.github.com>
2022-07-20 19:20:50 +0200

    /.,mnbvcvbnm,./

---
+++ babel.config.js
@@ -0,0 +1,3 @@
+module.export = {
+    'preset': [['@babel/preset-env', { targets: {node: 'current' } }]],
+};
---
+++ lesson13/task2/calculator.js
@@ -0,0 +1,7 @@
+export const getSquaredArray = (arr) => 
+    arr.filter((num) => num * num);
+
+export const getOddNumbers = (arr) => 
+    arr.filter((num) => (num % 2 === 1));
+
+export default (a, b) => a + b;
